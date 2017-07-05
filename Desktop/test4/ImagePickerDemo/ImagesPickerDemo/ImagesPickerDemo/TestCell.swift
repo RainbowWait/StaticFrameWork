f9dafc100d115f7039d76d4704b433acfc5bf354
ZXY <1136200379@qq.com>
2017-07-05 16:36:07 +0800

    提交

---
+++ Desktop/test4/ImagePickerDemo/ImagesPickerDemo/ImagesPickerDemo/TestCell.swift
@@ -0,0 +1,19 @@
+//
+//  TestCell.swift
+//  ImagesPickerDemo
+//
+//  Created by 郑小燕 on 2017/6/27.
+//  Copyright © 2017年 郑小燕. All rights reserved.
+//
+
+import UIKit
+
+class TestCell: UICollectionViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
